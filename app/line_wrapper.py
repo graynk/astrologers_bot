a2a2caf9d9946cd9dae95cb5e5b7734fe0d8db4a
graynk <gray.nk@gmail.com>
2020-09-13 17:01:08 +0500

    враппер уважает переносы строк

--- app/line_wrapper.py
+++ app/line_wrapper.py
@@ -1,26 +1,14 @@
 from textwrap import TextWrapper
-import re
-
 
 class DocumentWrapper(TextWrapper):
 
     def wrap(self, text):
-        para_edge = re.compile(r'(\n\s*\n)', re.MULTILINE)
-        paragraphs = para_edge.split(text)
+        split_text = text.split('\n')
         wrapped_lines = []
-        for para in paragraphs:
-            if para.isspace():
-                if not self.replace_whitespace:
-                    # Do not take the leading and trailing newlines since
-                    # joining the list with newlines (as self.fill will do)
-                    # will put them back in.
-                    if self.expand_tabs:
-                        para = para.expandtabs()
-                    wrapped_lines.append(para[1:-1])
-                else:
-                    # self.fill will end up putting in the needed newline to
-                    # space out the paragraphs
-                    wrapped_lines.append('')
-            else:
-                wrapped_lines.extend(TextWrapper.wrap(self, para))
+        for line in split_text:
+            if line == '':
+                wrapped_lines.append(line)
+                continue
+            wrapped_lines.extend(TextWrapper.wrap(self, line))
+
         return '       \n'.join(wrapped_lines) + '       '
